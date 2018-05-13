2c2903ffa0c8948114b266ae5ddf8f7f8e191a32
junfff <james922@163.com>
2018-05-14 00:10:43 +0800

    update--> 登陆 聊天 退出 ok

--- mychat/myserver/include/chat_fifo.h
+++ mychat/myserver/include/chat_fifo.h
@@ -9,4 +9,10 @@ c_msg *ReadFifo(char *fifo_name);
 
 void *json_to_struct(cJSON* json_obj);
 
+int SendMsg(char *fifo_path,c_msg *to_msg);
+
+void GetFifoPath(my_usr info,char *fifo_path);
+
+cJSON *struct_to_json(void* struct_obj);
+
 #endif
