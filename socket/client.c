85642fbc7580371912ff57e1406ae8738698546f
junfff <james922@163.com>
2018-06-10 01:20:50 +0800

    update--> server_pthread

--- socket/client.c
+++ socket/client.c
@@ -14,26 +14,26 @@
 int main()
 {
 	int lfd,ret;
-	int backlog = 100;
-  struct sockaddr_in addr;
-  char buf[BUFSIZ];
+	//int backlog = 100;
+  	struct sockaddr_in addr;
+  	char buf[BUFSIZ];
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(SERVER_PORT);
 	//addr.sin_addr.s_addr = htonl(INADDR_ANY);  //inet_pton(SERVER_IP);
 	//addr.sin_addr.s_addr = inet_pton(SERVER_IP);
-  int intip = 0;
+  	int intip = 0;
 	inet_pton(AF_INET,SERVER_IP,&intip);
 	addr.sin_addr.s_addr = intip;
 
 	//int socket(int domain, int type, int protocol);
-  lfd = socket(AF_INET,SOCK_STREAM,0);
+  	lfd = socket(AF_INET,SOCK_STREAM,0);
 	if(lfd == -1)
 	{
 		printf("socket error !!");
 		exit(1);
 	}
-  
+
 
 
 	printf("client connect !!lfd = %d\n",lfd);
@@ -51,28 +51,28 @@ int main()
 		printf("client start send char !!!ret = %d lfd = %d\n",ret,lfd);
 
 		fgets(buf,sizeof(buf),stdin);
-	ret = write(lfd,buf,strlen(buf));
-	if(ret == -1)
-	{
-		printf("write error !!");
-		exit(1);
-	}
-	printf("client send ret = %d\n}",ret);
+		ret = write(lfd,buf,strlen(buf));
+		if(ret == -1)
+		{
+			printf("write error !!");
+			exit(1);
+		}
+		printf("client send ret = %d\n}",ret);
 
-	 //ssize_t read(int fd, void *buf, size_t count);
-  ret = read(lfd,buf,sizeof(buf));
-	if(ret == -1)
-	{
-		printf("read error !!");
-		exit(1);
-	}
+	 	//ssize_t read(int fd, void *buf, size_t count);
+  		ret = read(lfd,buf,sizeof(buf));
+		if(ret == -1)
+		{
+			printf("read error !!");
+			exit(1);
+		}
 
-	 printf("client rev buf is:%s\n",buf);
+	 	printf("client rev buf is:%s\n",buf);
 
-	 sleep(1);
+	 	sleep(1);
 	}
 	close(lfd);
 
 	printf("client finish !!");
-   return 0;
+   	return 0;
 }
---
+++ socket/server_pthread.c
@@ -0,0 +1,21 @@
+/*================================================================
+*   Copyright (C) 2018 LiuJunFeng Ltd. All rights reserved.
+*   
+*   文件名称：server_pthread.c
+*   创 建 者：LiuJunFeng
+*   创建日期：2018年06月09日 22:05(Saturday) 
+*   描    述：
+*
+================================================================*/
+
+#include <stdio.h>
+
+#include "wrap.h"
+
+int main()
+{
+
+	printf("main start!!");
+
+	return 0;
+}
