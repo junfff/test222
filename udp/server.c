58cdf0a8d60e5fa6f6720d7453be0eeb534ba0c0
junfff <james922@163.com>
2018-06-27 00:00:15 +0800

    update--> udp-6-26

---
+++ udp/server.c
@@ -0,0 +1,67 @@
+/*================================================================
+ *   Copyright (C) 2018 LiuJunFeng Ltd. All rights reserved.
+ *   
+ *   文件名称：server.c
+ *   创 建 者：LiuJunFeng
+ *   创建日期：2018年06月26日 23:08(Tuesday) 
+ *   描    述：
+ *
+ ================================================================*/
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+//d#include <sys/types.h>
+//#include <sys/socket.h>
+
+#define SERV_PORT 33000
+
+int main()
+{
+	struct sockaddr_in serv_addr, clie_addr;
+	socklen_t clie_addr_len;
+	int sockfd;
+	char buf[BUFSIZ];
+	char str[INET_ADDRSTRLEN];
+	int i,n;
+
+	sockfd = socket(AF_INET,SOCK_DGRAM,0);
+
+	bzero(&serv_addr,sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(SERV_PORT);
+
+	bind(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr));
+
+	printf("Accepting connections ...\n");
+	while(1)
+	{
+		clie_addr_len = sizeof(clie_addr);
+		n = recvfrom(sockfd,buf,BUFSIZ,0,(struct sockaddr *)&clie_addr,&clie_addr_len);
+		if(n == -1)
+		{
+			perror("recvfrom error");
+		}
+
+		printf("received from %s at PORT %d\n",
+					inet_ntop(AF_INET,&clie_addr.sin_addr,str,sizeof(str)),
+					ntohs(clie_addr.sin_port));
+
+		for(i=0;i<n;i++)
+		{
+			buf[i] = toupper(buf[i]);
+		}
+
+		n = sendto(sockfd,buf,n,0,(struct sockaddr *)&clie_addr,sizeof(clie_addr));
+		if(n == -1)
+		{
+			perror("sendto error");
+		}
+	}
+
+	close(sockfd);
+	return 0;
+}
