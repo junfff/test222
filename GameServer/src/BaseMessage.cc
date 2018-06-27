b7a3e6648002000dc6d51c44979bcdcb5c55c317
junfff <james922@163.com>
2018-06-27 16:17:43 +0800

    update--> gameserver

---
+++ GameServer/src/BaseMessage.cc
@@ -0,0 +1,18 @@
+/*================================================================
+ *   Copyright (C) 2018 LiuJunFeng Ltd. All rights reserved.
+ *   
+ *   文件名称：BaseMseeage.c
+ *   创 建 者：LiuJunFeng
+ *   创建日期：2018年06月27日 10:49(Wednesday) 
+ *   描    述：
+ *
+ ================================================================*/
+#include <BaseMessage.h>
+#include <stdlib.h>
+char *BaseMessage:: GetByte()
+{
+	char *buf = (char *)malloc(sizeof(char *)*123);
+
+
+	return buf;
+}
