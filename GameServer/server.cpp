8cd5f72d5bf21bbfcb7bd64f10948c0f7051e703
junfff <james922@163.com>
2018-06-27 23:12:52 +0800

    update--> game-threadpool-ok

--- GameServer/server.cpp
+++ GameServer/server.cpp
@@ -11,19 +11,23 @@
 #include "./include/threadpool.h"
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
+
+
+
 int main(int argc,char *argv[])
 {
-	//threadpool_t *thp = threadpool_create(3,100,100); // 创建 线程  最小线程，最大，队列最大
-	//printf("pool inited\n");
+	threadpool_t *thp = threadpool_create(3,100,100); // 创建 线程  最小线程，最大，队列最大
+	printf("pool inited\n");
 
 	//printf("add task %d\n",i);
 	//threadpool_add(thp,process,(void *)&num[i]);
 
 
-    //Epoll_Start(argc,argv,thp);
+    Epoll_Start(argc,argv,thp);
 
-	//printf("server end >>>>>>>>>>>>\n");
-	//threadpool_destroy(thp);
-	Start();
+	printf("server end >>>>>>>>>>>>\n");
+	threadpool_destroy(thp);
+
 	return 0;
 }
