f8a068d488b2c9baaaff32a36f0157b529e11899
junfff <james922@163.com>
2018-08-28 23:59:53 +0800

    update--> 828-kelly2

---
+++ regex/while.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+#================================================================
+#   Copyright (C) 2018 LiuJunFeng Ltd. All rights reserved.
+#   
+#   文件名称：while.sh
+#   创 建 者：LiuJunFeng
+#   创建日期：2018年07月03日 23:44(Tuesday) 
+#   描    述：
+#
+#================================================================
+try=NULL
+while [ "$try" != "123" ]
+do
+	if [ "$try" != NULL ]
+	then
+		echo "sorry try again !!"
+	fi
+	read try
+done
+
+echo "you are ok !!"
+
+
+
